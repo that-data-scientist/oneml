8537db614c5ca3854e74867efa8dd42f44033460
ritabrata <ritabrata.m@rapido.bike>
2021-09-14 10:02:27 +0530

    Refactor imputing strategies and remove redundant if else loop

--- src/data/imputer.py
+++ src/data/imputer.py
@@ -5,38 +5,50 @@ from src.utils.console import log_step
 
 class Imputer():
     def __init__(self):
-        self.mode_imputer = ModeImputer()
-        self.mean_imputer = MeanImputer()
+        mode_imputer = ModeImputer()
+        mean_imputer = MeanImputer()
+        self.imputing_strategies = [mode_imputer, mean_imputer]
 
     @log_step
     def handle_missing_values(self, df: pd.DataFrame) -> pd.DataFrame:
         missing_value_pct = missing_value_percentage(df)
 
-        if missing_value_pct.sum() == 0:
-            return df
-
-        if missing_value_pct.sum() < 0.1:
+        if missing_value_pct.sum() == 0 or missing_value_pct.sum() < 0.1:
             return df.dropna()
 
         for k, v in missing_value_pct.items():
-            if pd.api.types.is_string_dtype(df[k]):
-                df[k] = self.mode_imputer.impute(df[k])
-            elif pd.api.types.is_numeric_dtype(df[k]):
-                df[k] = self.mean_imputer.impute(df[k])
+            for imputing_strategy in self.imputing_strategies:
+                if imputing_strategy.check(df[k]) is True:
+                    df[k] = imputing_strategy.impute(df[k])
         return df
 
 
 class ImputingStrategy():
+    def check(self, series: pd.Series) -> bool:
+        pass
+
     def impute(self, series: pd.Series) -> pd.Series:
         pass
 
 
 class ModeImputer(ImputingStrategy):
+    def check(self, series: pd.Series) -> bool:
+        if pd.api.types.is_string_dtype(series):
+            return True
+
+        return False
+
     def impute(self, series: pd.Series) -> pd.Series:
         return series.fillna(series.mode().iloc[0])
 
 
 class MeanImputer(ImputingStrategy):
+    def check(self, series: pd.Series) -> bool:
+        if pd.api.types.is_numeric_dtype(series):
+            return True
+
+        return False
+
     def impute(self, series: pd.Series) -> pd.Series:
         return series.fillna(series.mean())
 
