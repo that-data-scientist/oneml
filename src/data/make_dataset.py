e4e2fa971fc00e75afdff905eaac16da21630d39
ritabrata <ritabrata.m@rapido.bike>
2021-09-15 10:11:27 +0530

    Add exclude variables to make dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from src.data.imputer import Imputer
 from src.store.data_store import CsvStore
 from src.utils.config import load_config
@@ -11,9 +13,14 @@ def main():
     input_df = store.get_raw(project_name=config['project_name'], file_name=config['input_file_name'])
 
     processed_df = input_df \
-        .pipe(imputer.handle_missing_values, config['missing_values_drop_threshold'])
+        .pipe(imputer.handle_missing_values, config['missing_values_drop_threshold']) \
+        .pipe(exclude_variables, config['exclude_variables'])
 
     store.put_processed(project_name=config['project_name'], file_name='dataset.csv', df=processed_df)
+
+
+def exclude_variables(df: pd.DataFrame, exclude_variables: list) -> pd.DataFrame:
+    return df.drop(columns=exclude_variables)
 
 
 if __name__ == '__main__':
